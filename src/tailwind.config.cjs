91b8c5a7658890696ebf255aa5f330cf1ba4ec62
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2022-12-23 06:28:02 -0300

    💬 Atualização no local da página de visualizações

--- src/tailwind.config.cjs
+++ src/tailwind.config.cjs
@@ -3,7 +3,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: ["./view/**/*.{html,js,ejs}"],
+  content: ["./views/**/*.{html,js,ejs}"],
   theme: {
     extend: {
       fontFamily: {
