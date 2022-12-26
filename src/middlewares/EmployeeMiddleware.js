22b0a7dca659a8f1df9e050ad24b05b9f90936a4
Pedro Lucas <pedrolucas.jsrn@gmail.com>
2022-12-26 08:33:18 -0300

    🦺 Adicionado mediador de validação dos dados do funcionário

---
+++ src/middlewares/EmployeeMiddleware.js
@@ -0,0 +1,105 @@
+//! Importação dos módulos:
+//* Módulo de receber informações do corpo:
+import { body } from "express-validator";
+
+//* Módulo de serviço dos funcionários:
+import EmployeeService from "../services/EmployeeService.js";
+
+//! Criação da classe mediadora dos funcionários:
+class EmployeeMiddleware {
+    //* Método de validar os dados de criação:
+    create() {
+        //? Constante com a validação dos campos:
+        const create = [
+            body("firstName")
+                .trim()
+                .isLength({ min: 5, max: 55 })
+                .withMessage("Primeiro nome inválido"),
+            body("lastName")
+                .trim()
+                .isLength({ min: 5, max: 55 })
+                .withMessage("Último nome inválido"),
+            body("email")
+                .not()
+                .isEmpty()
+                .isEmail()
+                .withMessage("Email inválido"),
+            body("password")
+                .matches(
+                    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,24}$/
+                )
+                .withMessage("Senha inválida"),
+            body("confirmPassword").custom((value, { req }) => {
+                if (value !== req.body.password) {
+                    throw new Error("Senhas diferentes");
+                } else {
+                    return true;
+                }
+            }),
+            body("role")
+                .isIn(["Manager", "Manufacturer", "Deliveryman"])
+                .withMessage("Cargo inválido"),
+        ];
+
+        //* Retorno da constante de validação:
+        return create;
+    }
+
+    //* Método de validar os dados de atualização:
+    update() {
+        //? Constante com a validação dos campos:
+        const update = [
+            body("id").custom(async (value) => {
+                let result = await EmployeeService.view({ id: Number(value) });
+                if (result.message !== "Funcionário encontrado") {
+                    throw new Error("Identificador desconhecido");
+                }
+            }),
+            body("firstName")
+                .if(
+                    body("firstName").not().isEmpty() &&
+                        body("lastName").not().isEmpty()
+                )
+                .trim()
+                .isLength({ min: 5, max: 55 })
+                .withMessage("Primeiro nome inválido"),
+            body("lastName")
+                .if(
+                    body("lastName").not().isEmpty() &&
+                        body("firstName").not().isEmpty()
+                )
+                .trim()
+                .isLength({ min: 5, max: 55 })
+                .withMessage("Último nome inválido"),
+            body("email")
+                .if(body("email").not().isEmpty())
+                .isEmail()
+                .withMessage("Email inválido"),
+            body("password")
+                .if(body("password").not().isEmpty())
+                .matches(
+                    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,24}$/
+                )
+                .withMessage("Senha inválida"),
+            body("confirmPassword")
+                .if(body("password").not().isEmpty())
+                .custom((value, { req }) => {
+                    if (value !== req.body.password) {
+                        throw new Error("Senhas diferentes");
+                    } else {
+                        return true;
+                    }
+                }),
+            body("role")
+                .if(body("role").not().isEmpty())
+                .isIn(["Manager", "Manufacturer", "Deliveryman"])
+                .withMessage("Cargo inválido"),
+        ];
+
+        //* Retorno da constante de validação:
+        return update;
+    }
+}
+
+//* Exportação da classe instânciada:
+export default new EmployeeMiddleware();
