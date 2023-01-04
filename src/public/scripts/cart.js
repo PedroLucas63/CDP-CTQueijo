0160fd23a5f19770308c5ea32292a0f9e7c20269
Fernanda Azevedo <fernandadants@hotmail.com>
2023-01-04 10:54:30 -0300

    🙌 Finalizando validação de dados do Front-End

--- src/public/scripts/cart.js
+++ src/public/scripts/cart.js
@@ -1,6 +1,14 @@
-import IMask from 'imask';
-
-var phoneMask = IMask(
-    document.getElementById('phone'), {
-      mask: '+{55}(00)00000-0000'
+$(document).ready(() => {
+    var phoneMask = IMask(
+        document.getElementById('phone'), {
+        mask: '(00) 00000-0000'
     });
+    var cnpjMask = IMask(
+        document.getElementById('cnpj'), {
+        mask: '00.000.000/0000-00'
+    }); 
+    var cepMask = IMask(
+        document.getElementById('cep'), {
+        mask: '00000-000'
+    }); 
+})
